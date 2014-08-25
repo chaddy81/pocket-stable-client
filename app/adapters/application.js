74a6348b271edfd963ab411248c9316b4e57f59f
Chad Bartels <chad.bartels@gmail.com>
2014-08-25 18:51:38 -0400

    Hooking up to hosted api

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,7 +4,7 @@ import localStorage from 'simple-auth/stores/local-storage';
 
 export default DS.RESTAdapter.extend({
   namespace: 'api',
-  host: 'http://localhost:3000',
+  host: 'http://pocket-stable-api.herokuapp.com',
   headers: {
     'X-Auth-Token': JSON.parse(this.localStorage['ember_simple_auth:session']).user_token
   }
