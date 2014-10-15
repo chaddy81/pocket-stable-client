0b6ec1da0fe21a697d8470062aebde4ee5044351
Chad Bartels <chad.bartels@gmail.com>
2014-10-15 14:02:35 -0400

    Updating environments...again

--- config/environment.js
+++ config/environment.js
@@ -52,13 +52,13 @@ module.exports = function(environment) {
     ENV.APP.LOG_ACTIVE_GENERATION = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
     ENV.APP.LOG_TRANSITIONS = true;
-    ENV.APP.host = 'http://pocket-stable.herokuapp.com/';
+    ENV.APP.host = 'http://pocket-stable-api.herokuapp.com/';
     ENV.APP.serverTokenEndpoint = 'http://pocket-stable-api.herokuapp.com/users/sign_in';
     ENV.APP.crossOriginWhitelist = ['http://pocket-stable-api.herokuapp.com', 'http://pocket-stable-api.herokuapp.com/api/users'];
   }
 
   if (environment === 'staging') {
-    ENV.APP.host = 'http://pocket-stable.herokuapp.com/';
+    ENV.APP.host = 'http://pocket-stable-api.herokuapp.com/';
     ENV.APP.serverTokenEndpoint = 'http://pocket-stable-api.herokuapp.com/users/sign_in';
     ENV.APP.crossOriginWhitelist = ['http://pocket-stable-api.herokuapp.com', 'http://pocket-stable-api.herokuapp.com/api/users'];
   }
