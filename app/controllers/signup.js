b732daabe2298c203734ff98d486007a5f135079
Chad Bartels <chad.bartels@gmail.com>
2015-02-19 16:09:56 -0500

    Fixing upgrade changes

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -1,19 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  createUser: function() {
-    var name = this.get('name'),
-        email = this.get('email'),
-        password = this.get('password');
+  actions: {
+    createUser: function() {
+      var name = this.get('name'),
+          email = this.get('email'),
+          password = this.get('password');
 
-    var user = this.get('store').createRecord('user', { name: name, email: email, password: password, password_confirmation: password });
-    var self = this;
+      var user = this.get('store').createRecord('user', { name: name, email: email, password: password, password_confirmation: password });
+      var self = this;
 
-    user.save().then(function(data) {
-      console.log("Successful data: " + data);
-      self.transitionToRoute('login');
-    }, function(data) {
-      console.log(data);
-    }.bind(this));
+      user.save().then(function(data) {
+        console.log("Successful data: " + data);
+        self.transitionToRoute('login');
+      }, function(data) {
+        console.log(data);
+      }.bind(this));
+    }
   }
 });
